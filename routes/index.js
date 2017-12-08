e8fa484ec6a936e3907f862fe658f156d6fe870c
sxd <1447707563@qq.com>
2017-12-08 12:20:24 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+module.exports = function (app) {
+    app.get('/',function (req,res) {
+        res.render('index',{
+            title:'首页'
+        })
+    });
+
+    //注册页面
+    app.get('/reg',function (req,res) {
+        res.render('reg',{
+            title:'注册'
+        })
+    });
+    //注册行为
+    app.post('/reg',function (req,res) {
+
+    });
+
+    //登录页面
+    app.get('/login',function (req,res) {
+        res.render('login',{
+            title:'登录'
+        })
+    });
+    //登录行为
+    app.post('/login',function (req,res) {
+
+    });
+
+    //发表页面
+    app.get('/post',function (req,res) {
+        res.render('post',{
+            title:'发表'
+        })
+    });
+    //发表行为
+    app.post('/post',function (req,res) {
+
+    });
+
+    //退出
+    app.get('/logout',function (req,res) {
+
+    });
+}
